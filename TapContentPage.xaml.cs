ddb824dba1587df7a3c740e52d3787a8b6302733
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-20 19:10:31 -0600

    initial commit

---
+++ TapContentPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MauiTests
+{
+	public partial class TapContentPage : ContentPage
+	{
+		public TapContentPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
