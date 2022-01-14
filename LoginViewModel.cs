c668e4dec1dc1027ba53737fbb1b29cf7590b524
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2022-01-13 18:28:52 -0600

    ...

--- LoginViewModel.cs
+++ LoginViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace MauiTests
